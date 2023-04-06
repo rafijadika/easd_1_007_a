fc271882a91972a6aab9c4ccf87a719e73a94770
rafijadika <jadika.rafi@gmail.com>
2023-04-06 10:56:39 +0700

    Add project files.

---
+++ ExerciseAlgoritma1/ExerciseAlgoritma1.cpp
@@ -0,0 +1,20 @@
+//1. jelaskan, mengapa suatu algoritma dibutuhkan untuk menyelesaikan suatu masalah
+// karena didalam algoritma, terdapat berbagai macam,prosedur penyelesaian variable. oleh karena itu algoritma dibutuhkan
+//2. dalam algoritma, di klasifikasikan menjadi 2 data structur. sebutkan 2 data strutctur tersebut
+// static dan dynamic
+//3. untuk mengukur sebuah efisiensi waktu suatu algoritma dinutuhkan beberapa faktor. sebutkan faktor-faktor yang memengaruhi efisiensi waktu eksekusi suatu program
+// faktor-faktor yang mmempengaruhi antara lain factor time dan space
+//4. dari 6 algoritma yang sudah di pelajari. algoritma manakah yang paling mudah dipahami dan diimplementasikan untuk menggumpulkan data yang ukurannya besar. jelaskan kenapa anda memilih itu
+// 
+//5. sebutkan dari algoritma daru yang telah dipelajari mana yang  termasuk kedalam quadratic dan mana yang termasuk kedalam loglinear
+// 0(n2) dan0(n log n)
+//6. buatlah sebuah program yang mengimplementasikan mega sort mennggunakan bahasa c++. berikut merupakan algoritma mega sort
+//
+
+#include <iostream>
+using namespace std;
+
+int rafi[27];
+int cmp_count = 0; 
+int mov_count = 0; 
+int n;
